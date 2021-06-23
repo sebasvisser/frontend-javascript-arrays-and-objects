ae066f6b65d5678732539e1621b20bbee0506f72
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-06-23 21:34:56 +0200

    Opdrachten objects gemaakt

--- objects.js
+++ objects.js
@@ -2,11 +2,25 @@
 // Maak een object met daarin jouw voornaam (string), achternaam (string), leeftijd (getal) en geboortedatum (date object). De datum hoeft niet geformat te worden.
 // Log dit in de terminal.
 
+const userInfo = {
+  firstName: "Sebas",
+  lastName: "Visser",
+  age: 33,
+  dateOfBirth: new Date(1987, 7,8, 12,12,12,12),
+}
+console.log({userInfo});
 
 // ===  Opdracht 2 ===
 // Zorg ervoor dat de volgende string in de terminal wordt gelogd:
 // "Hoi! Ik ben <voornaam>, ik ben <leeftijd> jaar oud."
 
+console.log(
+    "Hoi ik ben " +
+    userInfo.firstName +
+    ", ik ben "  +
+    userInfo.age +
+    " jaar oud."
+)
 
 // === Opdracht 3: bekijk het volgende object ===
 
@@ -20,18 +34,27 @@ const product = {
 };
 
 // 3a. Log de naam van de serie in de terminal.
-
+console.log(product.productName);
 // 3b. Overschrijf de oude waarde van de property color ("creme") naar "roze". Log het object in de terminal om te checken of het gelukt is.
-
+product.color = "roze";
+console.log(product.color);
 // 3c. Log de tweede materiaalsoort ("kunststof") in de terminal
-
+console.log(product.material[1]);
 // 1d. Log de afmetingen (b-h-d) in de terminal
-
+console.log(product["b-h-d"]);
 
 // === Opdracht 4 ===
 // Declareer een variabele met daarin een string die het product beschrijft. Gebruik hiervoor de properties productName en material uit het object.
 // ---- Verwachte uitkomst:
 // "De Smeg Waterkoker is gemaakt van roestvrijstaal en kunststof"
 
+const description =
+    "De " +
+    product.productName +
+    " is gemaakt van " +
+    product.material[0] +
+    " en " +
+    product.material[1];
 
+console.log(description)
 
